686fd85562b83bd0676e1ece54e692092fa5380c
n-haruhi <hr23nakanishi@gmail.com>
2026-02-08 05:09:41 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/studytracker/study_time_tracker/StudyTimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.studytracker.study_time_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTimeTrackerApplication.class, args);
+	}
+
+}
